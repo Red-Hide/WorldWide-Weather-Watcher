fe9a89032927a17b81305dae18d3349e7abcc052
Red-Hide <guigui67480@gmail.com>
2023-10-27 14:41:53 +0200

    Combined configuration and ResetDefault for clarity

--- lib/Configuration/Configuration.cpp
+++ lib/Configuration/Configuration.cpp
@@ -1,5 +1,33 @@
 #include "Configuration.h"
 
+DS1307 clock;
+
+void ResetDefault(){
+    uint8_t addr = 0;
+    if(EEPROM.read(addr) == 255){
+        lum lum_config;
+        temp temp_config;
+        hygro hygro_config;
+        pression pression_config;
+        time time_config;
+        config config_values;
+        EEPROM.write(addr,1);
+        addr += 1;
+        EEPROM.put(addr,lum_config);
+        addr += sizeof(lum_config);
+        EEPROM.put(addr,temp_config);
+        addr += sizeof(temp_config);
+        EEPROM.put(addr,hygro_config);
+        addr += sizeof(hygro_config);
+        EEPROM.put(addr,pression_config);
+        addr += sizeof(pression_config);
+        EEPROM.put(addr,config_values);
+        clock.fillByYMD(time_config.year,time_config.month,time_config.day);
+        clock.fillByHMS(time_config.hours,time_config.minutes,time_config.seconds);
+        clock.fillDayOfWeek(time_config.dayOfW);
+    }
+}
+
 void Configuration(){
     if(digitalRead(redInterruptBtn)){
         unsigned long timeStart = millis();
--- lib/Configuration/Configuration.h
+++ lib/Configuration/Configuration.h
@@ -1,11 +1,64 @@
 #ifndef CONFIGURATION_H
 #define CONFIGURATION_H
+
 #define config_timeout 30
+#define lum_addr 1
+#define temp_addr lum_addr+sizeof(lum)
+#define hygro_addr temp_addr+sizeof(temp)
+#define pression_addr hygro_addr+sizeof(hygro)
+#define config_addr pression_addr+sizeof(pression)
+
 #include <EEPROM.h>
 #include "Configuration.h"
 #include "ChangeLEDStatus.h"
-#include "ResetDefault.h"
+#include <DS1307.h>
 
+typedef struct{
+    bool lumin = 1;
+    uint16_t lumin_low = 255;
+    uint16_t lumin_high = 768;
+}lum;
+
+typedef struct{
+    bool temp_air = 1;
+    int8_t min_temp_air = -10;
+    int8_t max_temp_air = 60;
+}temp;
+
+typedef struct
+{
+    bool hygr = 1;
+    int8_t hygr_mint = 0;
+    int8_t hygr_maxt = 50;
+}hygro;
+
+typedef struct
+{
+    bool pressure = 1;
+    uint16_t pressure_min = 850;
+    uint16_t pressure_max = 1080;
+}pression;
+
+typedef struct
+{
+    uint16_t year = 2023;
+    uint8_t month = 10;
+    uint8_t day = 25;
+    uint8_t hours = 12;
+    uint8_t minutes = 00;
+    uint8_t seconds = 00;
+    uint8_t dayOfW = WED;
+}time;
+
+typedef struct 
+{
+    uint8_t log_interval = 10;
+    uint16_t file_max_size = 4096;
+    uint8_t timeout = 30;
+    uint8_t version = 1;
+}config;
+
+void ResetDefault();
 void Configuration();
 void Update(String command, String value);
 
