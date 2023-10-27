ed978449540c6973c8851337bafc1fbd5762ec05
MELO <orfalim260@gmail.com>
2023-10-27 09:36:53 +0200

    CheckErrors SDCARD Errors Completed (To test)

---
+++ lib/CheckErrors/CheckErrors.cpp
@@ -0,0 +1,37 @@
+#include <Libraries.h>
+#include <CheckErrors.h>
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+ChainableLED leds(7, 8, 1);
+
+void SDCard_Errors() {
+    if (!card.init(SPI_HALF_SPEED, 4)) {
+        Serial.println("initialization failed. Things to check:");
+        Serial.println("* is a card inserted?");
+        Serial.println("* is your wiring correct?");
+        Serial.println("* did you change the chipSelect pin to match your shield or module?");
+        while (1);
+    } else {
+        Serial.println("Wiring is correct and a card is present.");
+    }
+
+  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
+
+    if (!volume.init(card)) {
+        Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
+        while (1);
+    }
+
+    uint32_t totalSpace = volume.blocksPerCluster() * volume.clusterCount() /1024;
+    uint32_t usedSpace = volume.blocksPerCluster() * (volume.clusterCount() - volume.fatCount()) /1024;
+
+    while (usedSpace >= totalSpace) {
+        Serial.println("SD card is full.");
+        leds.setColorRGB(7, 255, 0, 0); //rouge
+        delay(1000);
+        leds.setColorRGB(7, 255, 255, 255); //blanc
+        delay(1000);
+   }
+   }
+
---
+++ lib/CheckErrors/CheckErrors.h
@@ -0,0 +1,7 @@
+#ifndef CHECK_ERRORS_H
+#define CHECK_ERRORS_H
+
+void SDCard_Errors();
+
+
+#endif
