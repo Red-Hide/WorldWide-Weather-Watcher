35f3de668a13e913c2485096f21b34ec4012fb0b
Red-Hide <guigui67480@gmail.com>
2023-11-01 15:20:47 +0100

    Added file for functions that fetches data from sensors

---
+++ lib/Sensors/getData.cpp
@@ -0,0 +1,39 @@
+#include "Sensors.h"
+
+String getGPS()
+{
+    String gps = "G";
+    if (SoftSerial.available()) //Si il y a des valeurs a lire
+    {
+      int cond = 2;
+      String tmp; //caractère temporaire     String gps = "G";(à ajouter au string ou facilite la lecture)
+      while(cond > 0) //Premiere boucle, on attend d'obtenir les coordonnées (après 2 virgules)
+      {
+        tmp = char(SoftSerial.read()); //Lit un caractère ASCII de SoftSerial et le convertit
+        if (tmp == ",") //Quand on voit une virgule
+        {
+          cond -= 1;
+        }
+       
+      }
+      //A partir de maintenant, coordonnées GPS
+      cond = 1; //Reset de la condition
+      while(cond == 1) //Tant que l'on atteint pas la fin des coordonnées (le E de est ou W de Ouest)
+      {
+        tmp = char(SoftSerial.read()); //Lit un caractère ASCII de SoftSerial et le convertit
+        gps += tmp; //Ajout du caractère aux coordonnées
+        if (tmp == "E" or tmp == "W") //Quand on voit un E ou un W 
+        {
+          cond = 0; //Fin de la boucle
+          while (SoftSerial.available()) //Lit toutes les valeurs restantes
+          { 
+            SoftSerial.read(); //Permet de reset les valeurs de "SoftSerial"
+          }
+        }
+      }
+      
+    }
+    else
+        return "GNA"; //Renvoie une absence de valeurs
+    return gps; //Renvoie les coordonnées gps
+}
