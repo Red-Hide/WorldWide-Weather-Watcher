d44dcc79836bbf4a8551b9a926b82765110c920a
CedricHoog <cedric.hoog@gmail.com>
2023-11-01 15:31:17 +0100

    Updated data files
    Déplacé getLight() et getBME() de acquistion.cpp dans getData.cpp
    Supprimé GetDate.cpp
    Utilise "String("D" + clock.now())" dans acquisition.cpp pour obtenir la date en string, avec un D avant.

--- lib/Sensors/getData.cpp
+++ lib/Sensors/getData.cpp
@@ -37,3 +37,60 @@ String getGPS()
         return "GNA"; //Renvoie une absence de valeurs
     return gps; //Renvoie les coordonnées gps
 }
+
+String getLight()
+{
+    String clight = "NA";
+    if (LUMIN) //Si capteur de luminosité activé
+    {
+        int luminosite = analogRead(CapteurLumiere);
+        if (LUMIN_MIN > luminosite || luminosite > LUMIN_MAX)
+        {
+            *etat = erreur_valeur_capteur;
+            return "error"; 
+        }
+        clight = String(luminosite);
+    }
+    return "L" + clight;
+}
+
+String getBME() 
+{
+    float pression,temperature,hygrometrie;
+    String cpres = "NA";
+    String ctemp = "NA";
+    String chygr = "NA";
+
+    if (PRESSURE) //Si capteur de pression activé
+    {  
+        pression = bme.pres()/100; //Le capteur est lit en Pa, on converti en HPa
+        if (PRESSURE_MIN > pression || pression > PRESSURE_MAX)
+        {
+            *etat = erreur_valeur_capteur;
+            return "erreur";
+        }
+        cpres = String(pression);
+    }
+    if (TEMP_AIR) //Si capteur de temperature activé
+    {  
+        temperature = bme.temp(); // °C
+        if (TEMP_AIR_MIN > temperature || temperature > TEMP_AIR_MAX)
+        {
+            *etat = erreur_valeur_capteur;
+            return "erreur";
+        }
+        ctemp = String(temperature);
+    }
+    if (HYGR) //Si capteur d'hygrométrie activé
+    {  
+        hygrometrie = bme.hum(); // %
+        if (HYGR_MIN > hygrometrie || hygrometrie > HYGR_MAX)
+        {
+            *etat = erreur_valeur_capteur;
+            return "erreur"; 
+        }
+        chygr = String(hygrometrie);
+    }
+    
+    return "P" + cpres + "T" + ctemp + "H" + chygr;
+}
--- src/acquisition.cpp
+++ src/acquisition.cpp
@@ -11,85 +11,10 @@ String getLight();
 void acquision()
 {
     //Ligne de mesure : D + (date) + G + (Position) + T  + (Temperature) + H + (Humidite) + P + (Pression) + L  + (luminosite)
-    String LigneMesure = getDate() + getBME() + getLight();
+    String LigneMesure = String("D" + clock.now()) + getGPS() + getBME() + getLight();
 
     SDCardWrite(LigneMesure);
 
     //voir comment fonctionne le gps et l'heure
 }
 
-String getLight()
-{
-    String clight = "NA";
-    if (LUMIN) //Si capteur de luminosité activé
-    {
-        int luminosite = analogRead(CapteurLumiere);
-        if (LUMIN_MIN > luminosite || luminosite > LUMIN_MAX)
-        {
-            *etat = erreur_valeur_capteur;
-            return "error"; 
-        }
-        clight = String(luminosite);
-    }
-    return "L" + clight;
-}
-
-String getBME() 
-{
-    float pression,temperature,hygrometrie;
-    String cpres = "NA";
-    String ctemp = "NA";
-    String chygr = "NA";
-
-    if (PRESSURE) //Si capteur de pression activé
-    {  
-        pression = bme.pres()/100; //Le capteur est lit en Pa, on converti en HPa
-        if (PRESSURE_MIN > pression || pression > PRESSURE_MAX)
-        {
-            *etat = erreur_valeur_capteur;
-            return "erreur";
-        }
-        cpres = String(pression);
-    }
-    if (TEMP_AIR) //Si capteur de temperature activé
-    {  
-        temperature = bme.temp(); // °C
-        if (TEMP_AIR_MIN > temperature || temperature > TEMP_AIR_MAX)
-        {
-            *etat = erreur_valeur_capteur;
-            return "erreur";
-        }
-        ctemp = String(temperature);
-    }
-    if (HYGR) //Si capteur d'hygrométrie activé
-    {  
-        hygrometrie = bme.hum(); // %
-        if (HYGR_MIN > hygrometrie || hygrometrie > HYGR_MAX)
-        {
-            *etat = erreur_valeur_capteur;
-            return "erreur"; 
-        }
-        chygr = String(hygrometrie);
-    }
-    
-    return "P" + cpres + "T" + ctemp + "H" + chygr;
-}
-
-
-DS1307 clock; 
-
-String getDate() {
-    clock.begin(); 
-    String date = "";
-
-
-    clock.getTime();
-
-    String year = String(clock.year, DEC);
-    String month = String(clock.month, DEC);
-    String day = String(clock.dayOfMonth, DEC);
-
-    date = year + "/" + month + "/" + day;
-
-    return "D" + date;
-}
