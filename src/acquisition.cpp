8f648973944f67d05f66a2b4ab4b0d6919b11a59
CedricHoog <cedric.hoog@gmail.com>
2023-11-02 16:30:41 +0100

    Update acquisition.cpp
    BONNE VERSION
    Gère le mode éco
    Change la structure d'une ligne de mesure (GPS à la fin)
    Renvoie la ligne de donnée
    getDate : remis à la lettre "D" (T etait la temperature)

--- src/acquisition.cpp
+++ src/acquisition.cpp
@@ -1,20 +1,28 @@
 #include <Libraries.h>
-
-
 BME280I2C bme;
-
 String getDate();
 String getGPS();
 String getBME();
 String getLight();
 
-void acquision()
+String acquisition()
 {
-    //Ligne de mesure : D + (date) + G + (Position) + T  + (Temperature) + H + (Humidite) + P + (Pression) + L  + (luminosite)
-    String LigneMesure = String("D" + clock.now()) + getGPS() + getBME() + getLight();
-
-    SDCardWrite(LigneMesure);
-
-    //voir comment fonctionne le gps et l'heure
+    String data_line;
+    switch (state)
+        case economique:
+            static bool GPS_checked_before = false;
+            if (GPS_checked_before) 
+            {
+                GPS_checked_before = false;
+                data_line = getDate()  + getBME() + getLight() + "G : ECO";
+            }
+            else 
+            {
+                GPS_checked_before = true;
+                data_line = getDate()  + getBME() + getLight() + getGPS();
+            }
+        default :
+            data_line = getDate()  + getBME() + getLight() + getGPS();
+    //Ligne de mesure : D : + (date)  + T : + (Temperature) + H : + (Humidite) + P : + (Pression) + L : + (luminosite) + G : + (Position)
+    return data_line;
 }
-
