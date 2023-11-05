f33a0bca579985e053a915feebe086c53886579c
Red-Hide <guigui67480@gmail.com>
2023-11-05 12:27:17 +0100

    Reverted using main()

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,39 @@
 #include "Sensors.h"
 #include <EEPROM.h>
 
-int main(){
+void setup()
+{
   initSensors();
   ResetDefault();
   Configuration();
-  for (;;)
-  {
-    unsigned long TimeStart = millis();
-    uint8_t interval;
-    if (state == economique)
-    {
-      interval = EEPROM.read(config_addr) * 2;
-    }else{
-      interval = EEPROM.read(config_addr);
-    }
-    
-    while (millis() - TimeStart < interval * 60UL * 1000UL){}
-    
-    String mesures;
-    mesures += acquisition();
+}
 
-    if (state == maintenance){Serial.println(mesures);}else{SDCardWrite(mesures);}
+void loop()
+{
+  unsigned long TimeStart = millis();
+  uint8_t interval;
+  if (state == economique)
+  {
+    interval = EEPROM.read(config_addr) * 2;
+  }
+  else
+  {
+    interval = EEPROM.read(config_addr);
+  }
+
+  while (millis() - TimeStart < interval * 60UL * 1000UL)
+  {
+  }
+
+  String mesures;
+  mesures += acquisition();
+
+  if (state == maintenance)
+  {
+    Serial.println(mesures);
+  }
+  else
+  {
+    SDCardWrite(mesures);
   }
 }
